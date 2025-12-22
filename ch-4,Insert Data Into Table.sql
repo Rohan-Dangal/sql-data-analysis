12e5fd458f1e043342b1ec61c0a4e7bb6e94308c
Rohan Dangal <johndangal2061@gmail.com>
2025-12-22 11:42:08 +0545

    Add files via upload

---
+++ ch-4,Insert Data Into Table.sql
@@ -0,0 +1,15 @@
+--INSERTING DATA INTO THE TABLE
+
+SELECT * FROM EMPLOYEE;
+INSERT into employee(name,position,department,hire_date,salary,address)
+VALUES('Rohan Dangal','Data Analyst','Data Science','2025-12-02',148000.00,'Damak'),
+
+  ('Sarah Jones','Data Scientist','Analytics','2024-05-10',160000.00,'Kathmandu'),
+  ('Michael Chen','Data Engineer','Engineering','2023-08-20',155000.00,'Pokhara'),
+  ('Emily Williams','Business Analyst','Marketing','2025-01-15',130000.00,'Lalitpur'),
+('David Brown','BI Developer','IT','2022-11-25',145000.00,'Biratnagar');
+
+
+
+
+
